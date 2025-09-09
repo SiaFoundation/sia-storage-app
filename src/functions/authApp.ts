a4e0d06147a1d68be2c3089442501213d836abbc
John Williams <john@sia.tech>
2025-09-09 13:16:47 -0700

    refactor: extend account flow pattern, styling

---
+++ src/functions/authApp.ts
@@ -0,0 +1,44 @@
+import { Linking } from 'react-native'
+import InAppBrowser from 'react-native-inappbrowser-reborn'
+
+export default async function authApp(url: string) {
+  if (await InAppBrowser.isAvailable()) {
+    const sub = Linking.addEventListener('url', ({ url: recievedURL }) => {
+      if (recievedURL.includes('siastorage://')) {
+        console.log(recievedURL)
+        InAppBrowser.close()
+        sub.remove()
+      }
+    })
+
+    await InAppBrowser.open(url, {
+      // iOS Properties
+      dismissButtonStyle: 'cancel',
+      preferredBarTintColor: '#334155',
+      preferredControlTintColor: 'white',
+      readerMode: false,
+      animated: true,
+      modalPresentationStyle: 'fullScreen',
+      modalTransitionStyle: 'coverVertical',
+      modalEnabled: true,
+      enableBarCollapsing: false,
+      // Android Properties
+      showTitle: true,
+      toolbarColor: '#6200EE',
+      secondaryToolbarColor: 'black',
+      navigationBarColor: 'black',
+      navigationBarDividerColor: 'white',
+      enableUrlBarHiding: true,
+      enableDefaultShare: true,
+      forceCloseOnRedirection: false,
+      // Specify full animation resource identifier(package:anim/name)
+      // or only resource name(in case of animation bundled with app).
+      animations: {
+        startEnter: 'slide_in_right',
+        startExit: 'slide_out_left',
+        endEnter: 'slide_in_left',
+        endExit: 'slide_out_right',
+      },
+    })
+  }
+}
