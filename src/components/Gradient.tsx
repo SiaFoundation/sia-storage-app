eaa5e89d86b82f22b13bbf44b56dcc67a63eabbb
Alex Freska <alex.freska@gmail.com>
2025-10-05 11:50:35 -0400

    feat: gradient

---
+++ src/components/Gradient.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, StyleSheet, type ColorValue, type ViewProps } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+
+type Props = ViewProps & {
+  fadeTo?: 'top' | 'bottom'
+  overlayTopColor?: string
+  overlayBottomColor?: string
+}
+
+export function Gradient({
+  fadeTo = 'bottom',
+  overlayTopColor = 'rgba(16,18,21,0.66)',
+  overlayBottomColor = 'rgba(16,18,21,0)',
+  style,
+  ...rest
+}: Props) {
+  return (
+    <View style={[{ zIndex: 10, pointerEvents: 'none' }, style]} {...rest}>
+      <LinearGradient
+        colors={
+          (fadeTo === 'bottom'
+            ? [overlayTopColor, overlayBottomColor]
+            : [overlayBottomColor, overlayTopColor]) as [ColorValue, ColorValue]
+        }
+        start={{ x: 0.5, y: fadeTo === 'bottom' ? 0 : 1 }}
+        end={{ x: 0.5, y: fadeTo === 'bottom' ? 1 : 0 }}
+        style={StyleSheet.absoluteFillObject}
+      />
+    </View>
+  )
+}
