2571a6d4e76e88297c594a49c1436cea687d8e0d
Alex Freska <alex.freska@gmail.com>
2025-10-09 16:32:52 -0400

    refactor: rename db methods

--- src/screens/ImportFileScreen.tsx
+++ src/screens/ImportFileScreen.tsx
@@ -16,7 +16,7 @@ import {
 } from '../stacks/types'
 import { useToast } from '../lib/toastContext'
 import { useSdk } from '../stores/sdk'
-import { createFileRecord } from '../stores/files'
+import { insertOrReplaceFileRecord } from '../stores/files'
 import { uniqueId } from '../lib/uniqueId'
 import { FileDetailsImport } from '../components/FileDetailsImport'
 import { logger } from '../lib/logger'
@@ -72,7 +72,7 @@ export function ImportFileScreen({ route }: Props) {
     const indexerURL = await getIndexerURL()
     const pinnedObject = await sdk.pinShared(sharedObject.data)
     const sealedObject = pinnedObject.seal(await getAppKey())
-    await createFileRecord({
+    await insertOrReplaceFileRecord({
       ...sharedFile.data,
       createdAt: new Date().getTime(),
       sealedObjects: { [indexerURL]: sealedObject },
