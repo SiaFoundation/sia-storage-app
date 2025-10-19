a572be9ae88cf55ac5c15f6b6876de8520a68238
Alex Freska <alex.freska@gmail.com>
2025-10-19 13:17:09 -0400

    feat: swr hook helper args support

--- src/lib/swr.ts
+++ src/lib/swr.ts
@@ -9,16 +9,35 @@ import { mutate } from 'swr'
  * ```
  */
 export function buildSWRHelpers(baseKey: string) {
+  const changeCallbacks = new Map<string, () => void>()
+
+  const addChangeCallback = (key: string, callback: () => void) => {
+    changeCallbacks.set(key, callback)
+  }
+
+  const removeChangeCallback = (key: string) => {
+    changeCallbacks.delete(key)
+  }
+
   const getKey = (id?: string) => {
-    return id ? `${baseKey}/${id}` : `${baseKey}`
+    return id ? [`${baseKey}/${id}`] : [`${baseKey}`]
   }
+
   const triggerChange = async (keyPath?: string) => {
-    await mutate((key: string) => {
-      return typeof key === 'string' && key.startsWith(getKey(keyPath))
+    await mutate((key: string[]) => {
+      if (!Array.isArray(key) || typeof key[0] !== 'string') {
+        return false
+      }
+      const first = key[0]
+      return first.startsWith(getKey(keyPath)[0])
     })
+    changeCallbacks.forEach((callback) => callback())
   }
+
   return {
     getKey,
     triggerChange,
+    addChangeCallback,
+    removeChangeCallback,
   }
 }
