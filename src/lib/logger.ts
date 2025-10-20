43ec05e5bd157938389db10427819bb1527ab32e
Alex Freska <alex.freska@gmail.com>
2025-10-20 12:02:34 -0400

    feat: allow toggling sdk logs

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,21 +1,24 @@
 export const rustLogger = {
   hasInitialized: false,
   debug: (message: string) => {
-    logger.log('[rust][debug]', message)
+    logger.sdk('[rust][debug]', message)
   },
   info: (message: string) => {
-    logger.log('[rust][info]', message)
+    logger.sdk('[rust][info]', message)
   },
   warn: (message: string) => {
-    logger.log('[rust][warn]', message)
+    logger.sdk('[rust][warn]', message)
   },
   error: (message: string) => {
-    logger.log('[rust][error]', message)
+    logger.sdk('[rust][error]', message)
   },
 }
 
 export const logger = {
   log: (...args: any[]) => {
+    console.log(...args)
+  },
+  sdk: (...args: any[]) => {
     console.log(...args)
   },
   clear: () => {},
--- src/screens/SettingsLogsScreen.tsx
+++ src/screens/SettingsLogsScreen.tsx
@@ -1,22 +1,41 @@
 import { LogView } from '../components/LogView'
-import { clearLogs, useLogs } from '../stores/logs'
+import {
+  clearLogs,
+  toggleEnableSdkLogs,
+  useLogs,
+  useLogsStore,
+  useSDKLogsEnabled,
+} from '../stores/logs'
 import { type NativeStackScreenProps } from '@react-navigation/native-stack'
 import { type SettingsStackParamList } from '../stacks/types'
 import { SettingsLayout } from '../components/SettingsLayout'
 import { useSettingsHeader } from '../hooks/useSettingsHeader'
 import { BottomControlBar, iconColors } from '../components/BottomControlBar'
-import { TrashIcon } from 'lucide-react-native'
+import { EyeIcon, EyeOffIcon, TrashIcon } from 'lucide-react-native'
 
 type Props = NativeStackScreenProps<SettingsStackParamList, 'Logs'>
 
 export function SettingsLogsScreen({ navigation }: Props) {
   const logs = useLogs()
+  const sdkLogsEnabled = useSDKLogsEnabled()
   useSettingsHeader()
   return (
     <SettingsLayout>
       <LogView logs={logs} />
       <BottomControlBar
         width="dynamic"
+        right={[
+          {
+            id: 'enableSdkLogs',
+            label: 'SDK',
+            icon: sdkLogsEnabled ? (
+              <EyeIcon size={22} color={iconColors.white} />
+            ) : (
+              <EyeOffIcon size={22} color={iconColors.white} />
+            ),
+            onPress: toggleEnableSdkLogs,
+          },
+        ]}
         left={[
           {
             id: 'clear',
