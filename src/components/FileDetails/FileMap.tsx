9c0deb686620ff27157cd210dd523c8309b3e2da
John Williams <john@sia.tech>
2025-10-23 09:30:29 -0400

    feat: add slab map to file details

--- src/components/FileDetails/FileMap.tsx
+++ src/components/FileDetails/FileMap.tsx
@@ -1,34 +1,71 @@
-import { StyleSheet, Platform } from 'react-native'
-import MapView from 'react-native-maps'
+import React, { useMemo } from 'react'
+import { StyleSheet, View } from 'react-native'
+import useSWR from 'swr'
+import { AddressProtocol, Host } from 'react-native-sia'
+import { DEFAULT_INDEXER_URL } from '../../config'
+import { type FileRecord } from '../../stores/files'
+import { useHosts } from '../../stores/hosts'
+import { useSdk } from '../../stores/sdk'
+import Map from '../Map/Map'
+import { MapMarker } from '../Map/MapMarker'
+import { determineBestRegion } from '../Map/mapHelpers'
+import { SWROverlay } from '../SWROverlay'
 
-export function FileMap() {
+export function FileMap({ file }: { file: FileRecord }) {
+  const sdk = useSdk()
+  const hosts = useHosts()
+
+  const firstSlabID = useMemo(() => {
+    const slabs = file.objects[DEFAULT_INDEXER_URL]?.slabs ?? []
+    return slabs[0]?.id
+  }, [file])
+
+  const slab = useSWR(
+    sdk && firstSlabID ? ['sdk/slab', firstSlabID] : null,
+    () => sdk?.slab(firstSlabID!)
+  )
+
+  const matchingHosts: Host[] = useMemo(() => {
+    if (!hosts.data || !slab.data) return []
+    const keys = new Set(
+      (slab.data.sectors ?? []).map((sec: any) => sec.hostKey)
+    )
+    return hosts.data.filter((h) => keys.has(h.publicKey))
+  }, [hosts.data, slab.data])
+
+  const region = useMemo(() => {
+    if (matchingHosts.length === 0) return undefined
+    return determineBestRegion(matchingHosts)
+  }, [matchingHosts])
+
   return (
-    <MapView
-      style={styles.map}
-      mapType={Platform.OS === 'ios' ? 'mutedStandard' : 'standard'}
-      showsCompass={false}
-      showsScale={false}
-      showsIndoors={false}
-      showsTraffic={false}
-      zoomEnabled={false}
-      rotateEnabled={false}
-      pitchEnabled={false}
-      scrollEnabled={false}
-      toolbarEnabled={false}
-    >
-      {/* {file.slabs?.map((s) => (
-        <Marker
-          key={s.id}
-          coordinate={{ latitude: s.latitude, longitude: s.longitude }}
-        />
-      ))} */}
-    </MapView>
+    <View style={styles.container}>
+      <SWROverlay
+        response={hosts}
+        errorMessage="Failed to load hosts"
+        noDataMessage="No hosts available"
+      >
+        <Map region={region}>
+          {matchingHosts.map((h) => (
+            <MapMarker
+              size={10}
+              key={h.publicKey}
+              coordinate={{ latitude: h.latitude, longitude: h.longitude }}
+              title={
+                hosts.data
+                  ? h.addresses.find(
+                      (a) => a.protocol === AddressProtocol.SiaMux
+                    )?.address ?? h.publicKey
+                  : h.publicKey
+              }
+            />
+          ))}
+        </Map>
+      </SWROverlay>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  map: {
-    width: '100%',
-    height: 220,
-  },
+  container: { flex: 1 },
 })
