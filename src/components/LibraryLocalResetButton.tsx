8e604c4ada3ef71462fbb16b1b91593be99eedda
Alex Freska <alex.freska@gmail.com>
2025-11-03 13:43:17 -0800

    feat: reset data on db error button

---
+++ src/components/LibraryLocalResetButton.tsx
@@ -0,0 +1,38 @@
+import { Button } from './Button'
+import { Alert } from 'react-native'
+import { resetData } from '../stores/app'
+import { librarySwr } from '../stores/library'
+
+/**
+ * Button to reset the database and resync from the indexer.
+ * This button should almost never be shown to the user, only
+ * when there is a database query issue which should only
+ * be on test and dev builds.
+ */
+export function LibraryLocalResetButton() {
+  return (
+    <Button
+      variant="danger"
+      style={{ width: '100%', marginTop: 12 }}
+      onPress={() => {
+        Alert.alert(
+          'Reset Data',
+          'This will reset your library and resync from the indexer. This cannot be undone and you will lose any files that have not been uploaded. Continue?',
+          [
+            { text: 'Cancel', style: 'cancel' },
+            {
+              text: 'Reset',
+              style: 'destructive',
+              onPress: async () => {
+                await resetData()
+                librarySwr.triggerChange()
+              },
+            },
+          ]
+        )
+      }}
+    >
+      Reset library
+    </Button>
+  )
+}
