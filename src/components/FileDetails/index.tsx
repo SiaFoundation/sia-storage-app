23d8805f98bd928e3e68775881d54c9147110ea2
Alex Freska <alex.freska@gmail.com>
2025-11-13 15:07:31 -0800

    fix: file map label and temp disable only android

--- src/components/FileDetails/index.tsx
+++ src/components/FileDetails/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { View, StyleSheet, ScrollView, useWindowDimensions } from 'react-native'
+import { View, StyleSheet, ScrollView } from 'react-native'
 import { colors, palette } from '../../styles/colors'
 import { type FileRecord } from '../../stores/files'
 import { useFileStatus } from '../../lib/file'
 import { FileMeta } from './FileMeta'
-// import { FileMap } from './FileMap'
 
 export function FileDetails({
   file,
@@ -14,15 +13,11 @@ export function FileDetails({
   header?: React.ReactNode
 }) {
   const status = useFileStatus(file)
-  const { height: windowHeight } = useWindowDimensions()
 
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContent}>
         {header}
-        <View style={{ height: Math.round(windowHeight * 0.5) }}>
-          {/* <FileMap file={file} /> */}
-        </View>
         <View style={styles.metaContainer}>
           {status.data ? <FileMeta file={file} status={status.data} /> : null}
         </View>
@@ -35,7 +30,6 @@ const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: palette.gray[950] },
   scrollContent: { paddingBottom: 96 },
   metaContainer: {
-    marginTop: 16,
     backgroundColor: colors.bgElevated,
   },
 })
