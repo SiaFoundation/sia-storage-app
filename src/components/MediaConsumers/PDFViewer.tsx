ca359a57a3a15689cecff8ae512044e59585f080
Alex Freska <alex.freska@gmail.com>
2025-11-14 16:00:44 -0800

    fix: pdf viewer on android

--- src/components/MediaConsumers/PDFViewer.tsx
+++ src/components/MediaConsumers/PDFViewer.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, ViewStyle } from 'react-native'
+import { Platform, StyleSheet, ViewStyle } from 'react-native'
 import { WebView } from 'react-native-webview'
+import Pdf from 'react-native-pdf'
 import { useMemo } from 'react'
 
 export function PDFViewer({
@@ -14,6 +15,30 @@ export function PDFViewer({
     return source.replace(/[^/]+$/, '')
   }, [source])
 
+  const pdfSource = useMemo(
+    () => ({
+      uri: source,
+    }),
+    [source]
+  )
+
+  // The webview does not support PDF files on Android.
+  if (Platform.OS === 'android') {
+    return (
+      <Pdf
+        maxScale={4}
+        minScale={1}
+        spacing={8}
+        enablePaging
+        source={pdfSource}
+        style={[styles.webview, style]}
+        trustAllCerts={false}
+        enableAntialiasing
+      />
+    )
+  }
+
+  // Continue using the webview on other platforms because its nicer to use.
   return (
     <WebView
       style={[styles.webview, style]}
