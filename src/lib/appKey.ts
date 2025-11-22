2c13314a6ab21a4c5ff2f137ea04d85c362e95d0
Alex Freska <alex.freska@gmail.com>
2025-11-21 17:24:42 -0800

    feat: cache appkey for background tasks

--- src/lib/appKey.ts
+++ src/lib/appKey.ts
@@ -3,7 +3,16 @@ import { getRecoveryPhrase } from '../stores/settings'
 import { hexToUint8 } from './hex'
 import { APP_KEY } from '../config'
 
+// Cache the app key so that it can be accessed by background tasks.
+// Background tasks run with the suspended app state, but cannot access values in SecureStore.
+let appKey: AppKey | undefined = undefined
+
 export async function getAppKey(): Promise<AppKey> {
-  const seed = await getRecoveryPhrase()
-  return new AppKey(seed, hexToUint8(APP_KEY).buffer)
+  if (appKey) return appKey
+  const recoveryPhrase = await getRecoveryPhrase()
+  if (!recoveryPhrase) {
+    throw new Error('Recovery phrase not found')
+  }
+  appKey = new AppKey(recoveryPhrase, hexToUint8(APP_KEY).buffer)
+  return appKey
 }
