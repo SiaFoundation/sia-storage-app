399fb067d37b9157dcf0967d2d71f2d365565678
Alex Freska <alex.freska@gmail.com>
2025-12-18 10:43:28 -0800

    refactor: timeout, result, embedded browser utils

---
+++ src/lib/timeout.ts
@@ -0,0 +1,25 @@
+export class TimeoutError extends Error {
+  constructor(message = 'Connection timed out') {
+    super(message)
+    this.name = 'TimeoutError'
+  }
+}
+
+/**
+ * Wraps a promise with a timeout. Rejects with TimeoutError if not resolved in time.
+ */
+export function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(() => reject(new TimeoutError()), ms)
+    promise.then(
+      (value) => {
+        clearTimeout(timer)
+        resolve(value)
+      },
+      (err) => {
+        clearTimeout(timer)
+        reject(err)
+      }
+    )
+  })
+}
