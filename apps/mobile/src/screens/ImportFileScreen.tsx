a9ce2e1a3843e9573980e103e545f5b32a128bfe
Alex Freska <alex.freska@gmail.com>
2026-02-27 11:08:21 -0800

    fix: remove share URL scheme transforms now that SDK returns sia:// links

--- apps/mobile/src/screens/ImportFileScreen.tsx
+++ apps/mobile/src/screens/ImportFileScreen.tsx
@@ -3,7 +3,6 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { StyleSheet, View } from 'react-native'
 import { FileCarouselHeader } from '../components/FileCarousel/FileCarouselHeader'
 import { FileImport } from '../components/FileImport'
-import { convertSiaShareUrlToHttp } from '../lib/shareUrl'
 import type { ImportStackParamList, RootTabParamList } from '../stacks/types'
 import { colors } from '../styles/colors'
 
@@ -12,7 +11,7 @@ type Props = NativeStackScreenProps<ImportStackParamList, 'ImportFile'>
 export function ImportFileScreen({ route }: Props) {
   const navigation = useNavigation<NavigationProp<RootTabParamList>>()
   const id = route.params.id
-  const shareUrl = convertSiaShareUrlToHttp(route.params.shareUrl)
+  const shareUrl = route.params.shareUrl
 
   return (
     <View style={styles.container}>
